e38f9c1677e0c84b888a8726ec01e5856390b9db
Austin Cheng <austinjcheng@gmail.com>
2018-05-11 20:19:54 -0700

    Document additional classes

--- Page.java
+++ Page.java
@@ -3,29 +3,52 @@ public class Page {
 	private long guid;
 	private long size;
 	
+	/**
+	 * Constructor for the page class
+	 * @param number
+	 * @param guid
+	 * @param size
+	 */
 	public Page(int number, long guid, long size)
 	{
 		this.number = number;
 		this.guid = guid;
 		this.size = size;
 	}
-	
+	/**
+	 * Retuns the index of this page
+	 * @return
+	 */
 	public int getNumberofPage()
 	{
 		return this.number;
 	}
+	/**
+	 * Returns the last page in the file
+	 * @return
+	 */
 	public int getLastPage()
 	{
 		return 0;
 	}
-
+	/**
+	 * Getter method for the guid
+	 * @return
+	 */
 	public long getGUID() {
 		return this.guid;
 	}
-
+	/**
+	 * Getter method for the size
+	 * @return
+	 */
 	public long getSize() {
 		return this.getSize();
 	}
+	/**
+	 * Setter method for the page
+	 * @param number
+	 */
 	public void setPage(int number) {
 		this.number = number;
 	}
